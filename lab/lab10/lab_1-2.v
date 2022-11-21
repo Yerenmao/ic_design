fb09b310273baa8f0cc6b8588a6934e7a8fae262
Tristan Chen <tristanchen92@gmail.com>
2022-11-21 20:20:20 +0800

    modify lab_10

--- lab/lab10/lab_1-2.v
+++ lab/lab10/lab_1-2.v
@@ -9,18 +9,14 @@ wire Clock_Div_1Hz;
 wire Clock_Div_10000Hz;
 wire [3:0] Time;
 
-Frequency_Divider_1Hz(
+Frequency_Divider(
     .Clock(Clock), .Reset(Reset),
-    .Clock_Div_1Hz(Clock_Div_1Hz)
-);
-
-Frequency_Divider_10000Hz(
-    .Clock(Clock), .Clock_Div_10000Hz(Clock_Div_10000Hz)
+    .Clock_Div_1Hz(Clock_Div_1Hz), .Clock_Div_10000Hz(Clock_Div_10000Hz)
 );
 
 Traffic_Light(
     .Clock_Div_1Hz(Clock_Div_1Hz), .Clock_Div_10000Hz(Clock_Div_10000Hz),
-    .Dot_Row(Dot_Row), .Dot_Col(Dot_Col), .Time(Time);
+    .Dot_Row(Dot_Row), .Dot_Col(Dot_Col), .Time(Time)
 );
 
 Seven_Display(
@@ -151,20 +147,26 @@ end
 
 endmodule
 
-
-
-// implement frequency divider 1Hz
-module Frequency_Divider_1Hz(Clock, Reset, Clock_Div_1Hz);
+// implement frequency divider 10000Hz
+module Frequency_Divider_10000Hz(Clock, Reset, Clock_Div_1Hz, Clock_Div_10000Hz);
 
 input Clock, Reset;
-output reg Clock_Div_1Hz;
+output reg Clock_Div_1Hz, Clock_Div_10000Hz;
 
 reg [31:0] Count_1Hz;
+reg [15:0] Count_10000Hz;
 
 always @(posedge Clock or negedge Reset)
 begin
-    if(!Reset)
+    if(!Reset) begin
         Count_1Hz <= 0;
+        if(Count_10000Hz >= 16'd2500) begin
+            Count_10000Hz <= 0;
+            Clock_Div_10000Hz <= Count_10000Hz + 1;
+        end
+        else
+            Count_10000Hz <= Count_10000Hz + 1;
+    end
     else begin
         if(Count_1Hz >= 32'd25000000) begin
             Count_1Hz <= 0;
@@ -172,27 +174,13 @@ begin
         end
         else
             Count_1Hz <= Count_1Hz + 1;
+        if(Count_10000Hz >= 16'd2500) begin
+            Count_10000Hz <= 0;
+            Clock_Div_10000Hz <= ~Clock_Div_10000Hz;
+        end
+        else
+            Count_10000Hz <= Count_10000Hz + 1;
     end
-end
-
-endmodule
-
-// implement frequency divider 10000Hz
-module Frequency_Divider_10000Hz(Clock, Clock_Div_10000Hz);
-
-input Clock;
-output reg Clock_Div_10000Hz;
-
-reg [15:0] Count_10000Hz;
-
-always @(posedge Clock)
-begin
-    if(Count_10000Hz >= 16'd2500) begin
-        Count_10000Hz <= 0;
-        Clock_Div_10000Hz <= ~Clock_Div_10000Hz;
-    end
-    else
-        Count_10000Hz <= Count_10000Hz + 1;
 end
 
 endmodule
