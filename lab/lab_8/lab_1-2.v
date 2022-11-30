5a5a66c1765ea313f59cd1e0174c36bcfc47fb02
Tristan Chen <tristanchen92@gmail.com>
2022-11-30 15:36:32 +0800

    add lab_8

--- lab/lab_8/lab_1-2.v
+++ lab/lab_8/lab_1-2.v
@@ -5,40 +5,43 @@ input Clock, Reset, In;
 output [6:0] Out;
 
 wire Clock_Div;
-wire [2:0] TempOut;
+wire [2:0] CurrentState;
 
+freq_div_1hz(Clock, Reset, Clock_Div);
+Moore_Machine(Clock_Div, In, Reset, CurrentState);
+Seven_Display(CurrentState, Out);
 
+endmodule
 
-module
+// convert signal to 1Hz
+module freq_div_1hz(Clock, Reset, Clock_Div);
 
-// convert 4-bit input to seven display output
-module Seven_Display(Temp, Out);
+input Clock, Reset;
+output Clock_Div;
 
-input [2:0] TempOut;
-output reg [6:0] Out;
+reg [31:0] count;
 
-always @(TempOut)
-begin
-    case(TempOut)
-        0:  Out <= 7'b1000000;
-        1:  Out <= 7'b1111001;
-        2:  Out <= 7'b0100100;
-        3:  Out <= 7'b0110000;
-        4:  Out <= 7'b0011001;
-        5:  Out <= 7'b0010010;
-    default: Out <= 7'b1000000;
-    endcase
+always @(posedge Clock or negedge Reset) begin
+
+    if(!Reset)
+        count <= 0;
+    else begin
+        if(count >= 25000000) begin
+            count <= 0;
+            Clock_Div <= ~Clock_Div;
+        end
+        else 
+            count <= count + 1;
+    end
 end
 
 endmodule
 
-// implement moore machine
-module Moore_Machine(Clock_Div, In, Reset, TempOut);
+// convert 4-bit input to seven display output
+module Seven_Display(CurrentState, Out);
 
-input Clock_Div, In, Reset;
-output reg [2:0] TempOut;
-
-reg [2:0] CurrentState, NextState;
+input [2:0] CurrentState;
+output reg [6:0] Out;
 
 parameter [2:0]
     ST0 = 3'd0;
@@ -48,12 +51,78 @@ parameter [2:0]
     ST4 = 3'd4;
     ST5 = 3'd5;
 
-always @(posedge Clock_Div or negedge Reset)
+always @(CurrentState)
 begin
-    if(!Reset)
+    case(CurrentState)
+        ST0:  Out <= 7'b1000000;
+        ST1:  Out <= 7'b1111001;
+        ST2:  Out <= 7'b0100100;
+        ST3:  Out <= 7'b0110000;
+        ST4:  Out <= 7'b0011001;
+        ST5:  Out <= 7'b0010010;
+    default: Out <= 7'b1000000;
+    endcase
+end
+
+endmodule
+
+// implement moore machine
+module Moore_Machine(Clock_Div, In, Reset, CurrentState);
+
+input Clock_Div, In, Reset;
+output reg [2:0] CurrentState;
+
+reg [2:0] NextState;
+
+parameter [2:0]
+    ST0 = 3'd0;
+    ST1 = 3'd1;
+    ST2 = 3'd2;
+    ST3 = 3'd3;
+    ST4 = 3'd4;
+    ST5 = 3'd5;
+
+always @(posedge Clock_Div or negedge Reset) begin
+
+    if(!Reset) begin
         CurrentState <= ST0;
-    else
-        CurrentState <= 
+    end
+    else begin
+        CurrentState <= NextState;
+    end
+end
+
+always @(*) begin
+
+    if(In == 0) begin
+        case(CurrentState)
+            STO: NextState <= ST1;
+            ST1: NextState <= ST2;
+            ST2: NextState <= ST3;
+            ST3: NextState <= ST4;
+            ST4: NextState <= ST5;
+            ST5: NextState <= ST0;
+            default: NextState <= ST0;
+        endcase
+    end
+    if(In == 1) begin
+        case(CurrentState)
+            STO: NextState <= ST1;
+            ST1: NextState <= ST2;
+            ST2: NextState <= ST3;
+            ST3: NextState <= ST4;
+            ST4: NextState <= ST5;
+            ST5: NextState <= ST0;
+            default: NextState <= ST0;
+        endcase
+    end
+
+end
+
+endmodule
+
+
+
     
 
 
